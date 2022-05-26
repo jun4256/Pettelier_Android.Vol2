887d24ab895aafe2d27068eab56d54e542fba4ff
김준범 <diswk5167@naver.com>
2022-05-26 17:09:53 +0900

    게시판/홈/설정 페이지 이동

---
+++ app/src/main/java/com/example/pettelier_androidvol2/fm_home.java
@@ -0,0 +1,32 @@
+package com.example.pettelier_androidvol2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class fm_home extends Fragment {
+
+    private TextView tv_home;
+
+
+
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View fragment = inflater.inflate(R.layout.go_home, container, false);
+        tv_home = fragment.findViewById(R.id.tv_home);
+
+
+
+        return fragment;
+    }
+
+
+}

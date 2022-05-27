74ab21f30cca12ed91b13487a708d8fcf85c40b8
jun4256 <jun4256@naver.com>
2022-05-27 14:36:28 +0900

    0527  반려견 정보 수정페이지 만듦  시윤

---
+++ app/src/main/java/com/example/pettelier_androidvol2/pet_regi_retouch.java
@@ -0,0 +1,14 @@
+package com.example.pettelier_androidvol2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pet_regi_retouch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_regi_retouch);
+    }
+}

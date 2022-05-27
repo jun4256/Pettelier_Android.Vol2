974a3de9b5d0e204fc504704fc8db37d8692ee07
smhrd013 <19961119yun@gmail.com>
2022-05-27 12:10:40 +0900

    메인 좀 더 손봐야함

--- app/src/main/java/com/example/pettelier_androidvol2/Real_Main.java
+++ app/src/main/java/com/example/pettelier_androidvol2/Real_Main.java
@@ -31,4 +31,49 @@ public class Real_Main extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/c/mochamilk"));
         startActivity(intent);
     }
+
+    public void info10(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://animalmagazine.co.kr/bbs/board.php?bo_table=magazine&wr_id=239&sca=%EC%9D%B8%ED%8F%AC"));
+        startActivity(intent);
+    }
+
+    public void info9(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://witkorea.kr/board/knowhow/1568"));
+        startActivity(intent);
+    }
+
+    public void info8(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://bongus-studio.com/"));
+        startActivity(intent);
+    }
+
+    public void info7(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.k-health.com/news/articleView.html?idxno=55651"));
+        startActivity(intent);
+    }
+
+    public void info6(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://myanimals.co.kr/animals/the-benefits-of-children-and-pets-living-together/"));
+        startActivity(intent);
+    }
+
+    public void info5(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://blog.naver.com/chgg01/222521362582"));
+        startActivity(intent);
+    }
+
+    public void info4(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://hangang.seoul.go.kr/archives/61023"));
+        startActivity(intent);
+    }
+
+    public void info3(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://stylezineblog.com/4619"));
+        startActivity(intent);
+    }
+
+    public void info2(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://kfem.or.kr/?p=211296"));
+        startActivity(intent);
+    }
 }

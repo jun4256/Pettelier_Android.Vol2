20f337f8150db6e92a89af0fed6afbe7f192d063
gogo <goeun0311@gmail.com>
2022-05-30 15:38:55 +0900

    일단 회원가입은 가능함

--- app/src/main/java/com/example/pettelier_androidvol2/join.java
+++ app/src/main/java/com/example/pettelier_androidvol2/join.java
@@ -4,14 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class join extends AppCompatActivity {
 
     private EditText edt_id, edt_pw, edt_nick, edt_name, edt_phone, edt_adr, edt_adr2;
     private Button btn_confirm, btn_join, btn_cancel;
+    private RequestQueue requestQueue;
+    private StringRequest stringRequest;
 
 
     @Override
@@ -20,83 +42,119 @@ public class join extends AppCompatActivity {
         setContentView(R.layout.activity_join);
         Intent intent = getIntent();
 
+        // id, pw, nick, name, phone, addr -> 회원가입
         edt_id = findViewById(R.id.edt_id);
         edt_pw = findViewById(R.id.edt_pw);
         edt_nick = findViewById(R.id.edt_nick);
         edt_name = findViewById(R.id.edt_name);
         edt_phone = findViewById(R.id.edt_phone);
         edt_adr = findViewById(R.id.edt_adr);
-        edt_adr2 = findViewById(R.id.edt_adr2);
 
-        findViewById(R.id.btn_join).setOnClickListener(v -> {
-            doJoin();
+        // 중복확인
+        btn_confirm = findViewById(R.id.btn_confirm);
+
+
+        // 취소
+        btn_cancel = findViewById(R.id.btn_cancel);
+
+
+        //가입하기 버튼
+        btn_join = findViewById(R.id.btn_join);
+
+
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendRequest2();
+
+                Toast.makeText(getApplicationContext(),"실행고",Toast.LENGTH_SHORT).show();
+
+            }
         });
 
-        findViewById(R.id.btn_cancel).setOnClickListener(v -> {
-            finish();
-        });
+
+
     }
 
-    private void doJoin(){
-        String joinId = edt_id.getText().toString().trim();
-        String joinPw = edt_pw.getText().toString().trim();
-        String joinNick = edt_nick.getText().toString().trim();
-        String joinName = edt_name.getText().toString().trim();
-        String joinPhone = edt_phone.getText().toString().trim();
-        String joinAdr = edt_adr.getText().toString().trim();
-        String joinAdr2 = edt_adr2.getText().toString().trim();
 
-        if(joinId.length()==0){
-            Toast.makeText(this,"아이디를 입력해주세요.",Toast.LENGTH_SHORT).show();
-            edt_id.requestFocus();
+    private void sendRequest2() {
 
-            return;
-        }
+        //RequestQueue 객체 생성
+        requestQueue = Volley.newRequestQueue(this);    // this==getApplicationContext();
 
-        if(joinPw.length()==0){
-            Toast.makeText(this,"비밀번호를 입력해주세요.",Toast.LENGTH_SHORT).show();
-            edt_pw.requestFocus();
+        // 서버에 요청할 주소
+        String url = "http://218.149.140.51:8089/web/joinInsert.do";
 
-            return;
-        }
+        // 1.객체만들고 요청 주소만듦
 
-        if(joinNick.length()==0) {
-            Toast.makeText(this, "닉네임을 입력해주세요.", Toast.LENGTH_SHORT).show();
-            edt_nick.requestFocus();
-
-            return;
-        }
+        // 요청시 필요한 문자열 객체 생성  매개변수  4개(통신방식(get,post),요청url주소, new 리스너(익명클래스)-응답시필요한부분 작성함)
+        stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+            // 응답데이터를 받아오는 곳
+            // 응답시 데이터 받아오는 곳 - 통신이 잘됐다면 로그캣에서 확인하게출력함
+            @Override
+            public void onResponse(String response) {
+                Log.v("resultValue",response);
+                Log.v("resultValue", response.length()+"");         //응답글자 수 보여짐,
+                if(response.length() > 0) {
+                    Intent intent = new Intent(getApplicationContext(),Real_Main.class);
+                    startActivity(intent);
 
 
-        if(joinName.length()==0) {
-            Toast.makeText(this, "이름을 입력해주세요.", Toast.LENGTH_SHORT).show();
-            edt_name.requestFocus();
+                }else {
+                    // 실패
+                    Toast.makeText(getApplicationContext(), "가입 실패", Toast.LENGTH_SHORT).show();
+                }
 
-            return;
-        }
+            }
+        }, new Response.ErrorListener() {
+            // 서버와의 연동 에러시 출력
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+
+            }
+        }){
+            @Override //response를 UTF8로 변경해주는 소스코드
+            protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                try {
+                    String utf8String = new String(response.data, "UTF-8");
+                    return Response.success(utf8String, HttpHeaderParser.parseCacheHeaders(response));
+                } catch (UnsupportedEncodingException e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                } catch (Exception e) {
+                    // log error
+                    return Response.error(new ParseError(e));
+                }
+            }
+
+            // 보낼 데이터를 저장하는 곳 해쉬맵에 저장해서 보냄 - key,value
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                String joinId = edt_id.getText().toString();
+                String joinPw = edt_pw.getText().toString();
+                String joinNick = edt_nick.getText().toString();
+                String joinName = edt_name.getText().toString();
+                String joinPhone = edt_phone.getText().toString();
+                String joinAdr = edt_adr.getText().toString();
 
 
-        if(joinPhone.length()==0) {
-            Toast.makeText(this, "휴대폰번호를 입력해주세요.", Toast.LENGTH_SHORT).show();
-            edt_phone.requestFocus();
+                params.put("mb_id", joinId);
+                params.put("mb_pw",joinPw);
+                params.put("mb_nick",joinNick);
+                params.put("mb_name",joinName);
+                params.put("mb_phone",joinPhone);
+                params.put("mb_address",joinAdr);
+                // key값은 서버에서 지정한 name과 동일하게
 
-            return;
-        }
-
-
-        if(joinAdr.length()==0) {
-            Toast.makeText(this, "주소를 입력해주세요.", Toast.LENGTH_SHORT).show();
-            edt_adr.requestFocus();
-
-            return;
-        }
-
-        if(joinAdr2.length()==0) {
-            Toast.makeText(this, "상세주소를 입력해주세요.", Toast.LENGTH_SHORT).show();
-            edt_adr2.requestFocus();
-
-            return;
-        }
+                return params;
+            }
+        };
+        stringRequest.setTag("main");       //구분자 어떤클라이언트에서 요청했는지 나타냄 (중요하지않음)
+        requestQueue.add(stringRequest);        //실행 요청 add에 담으면 자동요청
 
     }
+
+
 }
